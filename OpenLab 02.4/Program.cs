752f9665dbb343d58dbd2db1a4bf123df59e6905
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:27:00 +0100

    Add project files.

---
+++ OpenLab 02.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OpenLab_02._4
+{
+    class Program
+    {
+        public static int GetLegsCount(int chickens, int cows, int pigs)
+        {
+            return ((chickens * 2) + (cows * 4) + (pigs * 4));
+        }
+        static void Main(string[] args)
+        {
+            int chickens = 1;
+            int cows = 1;
+            int pigs = 1;
+            Console.WriteLine(GetLegsCount(chickens, cows, pigs));
+        }
+    }
+}
